7a3767345c9e744644ad74061fe37774c31dbac7
trbharathwaj <31577161+trbharathwaj@users.noreply.github.com>
2018-10-30 12:10:35 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -76,10 +76,10 @@ def record_audio():
 
     p = pyaudio.PyAudio()
     stream = p.open(format=FORMAT,
-                    channels = 2,
+                    channels = 1,
                     rate = RATE,
                     input = True,
-                    input_device_index = 1,
+                    input_device_index = 2,
                     frames_per_buffer = CHUNK)
 
     print("* Now Recording")
